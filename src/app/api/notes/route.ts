9ce22b16d7d415aa6d90f8a900c76d0053a53168
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-08-21 17:37:00 +0900

    feat: 🎸 API と コンポーネントを実装

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,5 +1,6 @@
+import { zUpsertNote } from "@/app/notes/type";
 import { prisma } from "@/globals/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // 1. 動的レンダリングを強制する
 export const dynamic = 'force-dynamic';
@@ -9,4 +10,13 @@ export async function GET() {
   // 3. DBからノート一覧を取得
   const notes = await prisma.note.findMany();
   return NextResponse.json(notes)
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parsedData = zUpsertNote.parse(data);
+  const note = await prisma.note.create({
+    data: { title: parsedData.title, body: parsedData.body },
+  });
+  return new NextResponse(`${note.id}`, { status: 201 })
 }
