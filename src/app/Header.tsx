75341356be3f5dd1702a17bb2e6bf549b269a0cf
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-08-21 17:21:07 +0900

    一覧表示コンポーネントを実装した (#13)
    * feat: 🎸 $ yarn add --dev swr
    
    * feat: 🎸 コンポーネントを追加した

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -32,8 +32,7 @@ const Header: React.FC = () => {
           </Link>
           {/* 2. 画面幅が768px未満の場合は非表示 */}
           <nav className="hidden md:flex gap-12">
-            {/* 3. リンク先は未実装のためトップページに遷移 */}
-            <Link href="/" className="text-gray-600 hover:text-pink-500 active:text-pink-700 text-lg font-semibold transition duration-100">Memo</Link>
+            <Link href="/notes" className="text-gray-600 hover:text-pink-500 active:text-pink-700 text-lg font-semibold transition duration-100">Memo</Link>
             <Link href="/" className="text-gray-600 hover:text-pink-500 active:text-pink-700 text-lg font-semibold transition duration-100">FAQ</Link>
             <Link href="/" className="text-gray-600 hover:text-pink-500 active:text-pink-700 text-lg font-semibold transition duration-100">Setting</Link>
           </nav>
